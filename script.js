34e3861ce1fd0de4182e113ce895685f7d19c5e7
davronweb009 <davronweb07@mail.ru>
2025-02-07 19:25:02 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+//1. let user = 5 % 2
+// alert(user)
+
+
+//2. let number = Math.random() * 10
+// number = Math.ceil(number)
+// alert(number)
+
+
+//3.  alert(Math.floor(12.510))
+
+
+//4. let name = 'MARS IT SCHOOL'
+// admin = name.length;
+// alert(admin)
+
+
+//5.  for(let i = 0; i <= 9 ; i++){
+//     console.log('MARS IT SCHOOL')
+// }
+
+
+//6. let harflar = ["a" , "b" , "d" , "e" , "f" , "g" , "h" , "i" , "j" , "k" , "l" , "m" , "n" , "o" , "p" , "q" , "r" , "s" , "t",  "u" , "v" , "x" , "y" , "z" ]
+// console.log(harflar[2] + harflar[0] + harflar [20] + harflar[16] + harflar[13] + harflar[12])
+
+
+//7. let yosh = prompt("Yoshingizni kiriting:"); 
+//  user = parseInt(yosh);
+//  if (yosh > 18) {
+//      console.log("Siz balog‘at yoshiga yetgansiz.");
+//  } else if (yosh < 18) {
+//      console.log("Siz balog‘at yoshiga yetmagansiz.");
+//  } else {
+//      console.log("Balog‘at yoshi muborak!");
+//  }
+
+//8. let ism = prompt("Ismingizni kiriting:"); 
+// let teskari = ism.split("").reverse().join("");
+// console.log(teskari);
+
+
+
+//10.  let sonlar = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// console.log(sonlar)
+
+// let user = []
+
+// for (let i = 0; i < sonlar.length; i++) {
+//     if(sonlar[i] % 2 === 0){
+//     user.push(sonlar[i])
+// }
+// }
+// console.log(user)
